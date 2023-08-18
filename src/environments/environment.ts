214d58ef466311bf88c425998da2db82128bdc3a
abirami61299-pro <abirami@theproindia.com>
2023-08-18 17:08:41 +0530

    Fix : pro-gami version update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,9 +7,9 @@ export const environment = {
   baseUrl: 'https://gamifyapi.theproindia.com',
   blobEndpoint: 'https://prodgamificationui.blob.core.windows.net',
   containerName: 'sandbox-assets',
-  clientId: 'e7742fd0-da06-47ba-a6e8-96936cc7cbf9',
-  clientSecret: 'MGYzNTkyYzktMDU1MS00N2IzLWE4ZjEtMDQ3Mjg4ZTNmMjE5',
-  //Paste Your Application Code 
+  clientId: 'f9f95520-89d4-43b0-a326-affe40ff27f1',
+  clientSecret: 'pon8Q~dc-.o1wk~arnP6r9r3Zq8ls2emlMY1JbrW',
+  //Paste Your Application Code
   gamification: {
     userId: 'c7a09cc4-76e0-492c-b969-cd5c7a069ed0',
     gameId: '648967f30c6b3d35b04b1c73',

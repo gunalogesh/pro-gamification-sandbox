1431d517cb01896fb837328b6a72066737795245
abirami61299-pro <abirami@theproindia.com>
2023-09-19 16:20:24 +0530

    fixes

--- src/app/Pages/layout/layout.component.ts
+++ src/app/Pages/layout/layout.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { CodeChangeService } from 'src/app/Services/code-change.service';
+import { CodeChangeService } from '../../Services/code-change.service';
 
 @Component({
   selector: 'app-layout',
